aa1e8c120b7dc882fa1af13d7ab98f843772a254
csboo <csongiboo@gmail.com>
2024-05-14 10:33:15 +0200

    catch all errors in main (not handeled)

--- main.cpp
+++ main.cpp
@@ -1,26 +1,31 @@
+#include "kisbolt.h"
 #include <iostream>
 #include <vector>
-#include "kisbolt.h"
 
+// main fuggveny
+int main() {
+  try {
+    kisbolt coop;
 
-//main fuggveny
-int main(){
-  kisbolt coop;
-  coop.arucikk_hozzaadas("csoki", 50, 3);
-  coop.arucikk_hozzaadas("alma", 333, 4);
-  coop.arucikk_hozzaadas("bela", 200, 6);
+    // coop.arucikk_hozzaadas("csoki", 50, 3);
+    // coop.arucikk_hozzaadas("alma", 333, 4);
+    // coop.arucikk_hozzaadas("bela", 200, 6);
 
-  coop.arucikkek_listazasa();
-  coop.specifikus_arucikk_listazasa("alma");
-  
-  std::cout << coop.get_arucikkek().at(coop.arukereso("csoki")).get_instock();  
-  coop.feltoltes("csoki", 3);
-  std::cout << coop.get_arucikkek().at(coop.arukereso("csoki")).get_instock();  
-  std::cout << std::endl;
-  std::cout << coop.get_arucikkek().at(0).get_name();  
-  coop.arucikk_torlese("csoki");
-  std::cout << coop.get_arucikkek().at(0).get_name(); 
+    coop.arucikkek_listazasa();
+    coop.specifikus_arucikk_listazasa("alma");
 
-  
-  return 0;
+    std::cout << coop.get_arucikkek().at(coop.arukereso("csoki")).get_instock();
+    coop.feltoltes("csoki", 3);
+    std::cout << coop.get_arucikkek().at(coop.arukereso("csoki")).get_instock();
+    std::cout << std::endl;
+    std::cout << coop.get_arucikkek().at(0).get_name();
+    // coop.arucikk_torlese("csoki");
+    std::cout << coop.get_arucikkek().at(0).get_name();
+
+    coop.adatok_mentese();
+
+    return 0;
+  } catch(...){
+    std::cout << "error";
+  }
 }
