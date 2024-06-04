d6609d38cf3573f72e13ed831b840d10e8884f01
csboo <csongiboo@gmail.com>
2024-06-04 12:27:21 +0200

    deleted old files

--- product.cpp
+++
@@ -1,8 +0,0 @@
-//methods of product header:
-#include "product.h"
-
-product::product(std::string name, double price,unsigned int instock){
-  this->name = name;
-  this->price = price;
-  this->instock = instock;
-}
--- tools.h
+++
@@ -1,4 +0,0 @@
-#pragma once 
-#include <iostream>
-
-std::string sajat_to_lower(std::string string);
